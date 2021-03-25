f8249d370aafabc9dd52a19667db48c2026f0a48
Jason <jasonyata@gmail.com>
2021-03-25 15:49:24 -0700

    updated store, index, and root reducer files to include redux-persist for session storage

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
+
 import { Provider } from "react-redux";
-import store from "./redux/store";
 
 import "./index.css";
 import App from "./App";
@@ -12,9 +14,11 @@ import App from "./App";
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <React.StrictMode>
-                <App />
-            </React.StrictMode>
+            <PersistGate persistor={persistor}>
+                <React.StrictMode>
+                    <App />
+                </React.StrictMode>
+            </PersistGate>
         </BrowserRouter>
     </Provider>,
     document.getElementById("root")
