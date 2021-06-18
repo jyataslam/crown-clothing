1f8ae2a87f73033fbbffbe75a117f914ba68314e
Jason <jasonyata@gmail.com>
2021-06-18 15:43:47 -0700

    ethos section on home page complete

--- src/components/ethos/ethos.component.js
+++ src/components/ethos/ethos.component.js
@@ -6,6 +6,7 @@ import {
     Header,
     Subheader,
     IconsContainer,
+    EthoOpacityOverlay,
     EthosSection,
     EthoContainer,
     EthoBackgroundImage,
@@ -23,6 +24,9 @@ import Headshot from "../../assets/images/quote_headshot.jpg";
 import DayOneImage from "../../assets/images/pull-ups.jpg";
 import AccountabilityImage from "../../assets/images/accountability.jpg";
 import DisciplineImage from "../../assets/images/discipline.jpg";
+import FortyPercentImage from "../../assets/images/carrying_log.jpg";
+import HumilityImage from "../../assets/images/humility.jpg";
+import UncommonImage from "../../assets/images/uncommon.jpg";
 
 const Ethos = () => (
     <Main>
@@ -54,52 +58,91 @@ const Ethos = () => (
                 stands for.
             </Subheader>
             <IconsContainer>
-                <EthoContainer bgImage={DayOneImage}>
-                    <EthoBackgroundImage></EthoBackgroundImage>
+                <EthoContainer>
+                    <EthoOpacityOverlay />
+                    <EthoBackgroundImage
+                        bgImage={DayOneImage}
+                    ></EthoBackgroundImage>
                     <EthoHeader>Day One Mentality</EthoHeader>
                     <EthoDescription>
                         We live with a day one, week one mentality.
+                        <br></br>
+                        <br></br>
+                        <span>Day One Mentality</span>
                     </EthoDescription>
                 </EthoContainer>
-                <EthoContainer bgImage={DisciplineImage}>
-                    <EthoBackgroundImage></EthoBackgroundImage>
+                <EthoContainer>
+                    <EthoOpacityOverlay />
+                    <EthoBackgroundImage
+                        bgImage={DisciplineImage}
+                    ></EthoBackgroundImage>
                     <EthoHeader>Self-discipline</EthoHeader>
                     <EthoDescription>
                         It takes relentless self discipline to schedule
                         suffering into your day, every day.
+                        <br></br>
+                        <br></br>
+                        <span>Self-discipline</span>
                     </EthoDescription>
                 </EthoContainer>
-                <EthoContainer bgImage={AccountabilityImage}>
-                    <EthoBackgroundImage></EthoBackgroundImage>
+
+                <EthoContainer>
+                    <EthoOpacityOverlay />
+                    <EthoBackgroundImage
+                        bgImage={AccountabilityImage}
+                    ></EthoBackgroundImage>
                     <EthoHeader>personal accountability</EthoHeader>
                     <EthoDescription>
                         While most stop when they're tired, we stop when we're
                         done.
+                        <br></br>
+                        <br></br>
+                        <span>personal accountability</span>
                     </EthoDescription>
                 </EthoContainer>
-                <EthoContainer bgImage={AccountabilityImage}>
-                    <EthoBackgroundImage></EthoBackgroundImage>
-                    <EthoHeader>Humility</EthoHeader>
-                    <EthoDescription>
-                        To exist in this world, we must contend with
-                        humiliation, broken dreams, sadness, and loss.
-                    </EthoDescription>
-                </EthoContainer>
-                <EthoContainer bgImage={AccountabilityImage}>
-                    <EthoBackgroundImage></EthoBackgroundImage>
+                <EthoContainer>
+                    <EthoOpacityOverlay />
+                    <EthoBackgroundImage
+                        bgImage={FortyPercentImage}
+                    ></EthoBackgroundImage>
                     <EthoHeader>40% Rule</EthoHeader>
                     <EthoDescription>
                         No matter who you are, who your parents are or were,
                         where you live, what you do for a living, or how much
                         money you have, you’re probably living at about 40
                         percent of your true capability.
+                        <br></br>
+                        <br></br>
+                        <span>40% Rule</span>
                     </EthoDescription>
                 </EthoContainer>
-                <EthoContainer bgImage={AccountabilityImage}>
-                    <EthoBackgroundImage></EthoBackgroundImage>
-                    <EthoHeader>Stay Hard</EthoHeader>
+                <EthoContainer>
+                    <EthoOpacityOverlay />
+                    <EthoBackgroundImage
+                        bgImage={HumilityImage}
+                    ></EthoBackgroundImage>
+                    <EthoHeader>Humility</EthoHeader>
                     <EthoDescription>
-                        We live with a day one week one mentality.
+                        To exist in this world, we must contend with
+                        humiliation, broken dreams, sadness, and loss.
+                        <br></br>
+                        <br></br>
+                        <span>Humility</span>
+                    </EthoDescription>
+                </EthoContainer>
+                <EthoContainer>
+                    <EthoOpacityOverlay />
+                    <EthoBackgroundImage
+                        bgImage={UncommonImage}
+                    ></EthoBackgroundImage>
+                    <EthoHeader>Uncommon Amongst The Uncommon</EthoHeader>
+                    <EthoDescription>
+                        In a world where mediocrity is often the standard, our
+                        life’s mission is to become the uncommon amongst the
+                        uncommon.
+                        <br></br>
+                        <br></br>
+                        <span>Uncommon Amongst The Uncommon</span>
                     </EthoDescription>
                 </EthoContainer>
             </IconsContainer>
--- src/components/ethos/ethos.styles.js
+++ src/components/ethos/ethos.styles.js
@@ -41,50 +41,146 @@ export const Header = styled.h3`
     margin-bottom: 0;
     padding: 0 1rem;
     line-height: 1.2;
+
+    @media screen and (max-width: 800px) {
+        font-size: 3rem;
+    }
+    @media screen and (max-width: 550px) {
+        font-size: 2.8rem;
+    }
 `;
 
 export const Subheader = styled.p`
     text-align: center;
-    font-weight: 400;
+    font-weight: 200;
     color: #0c0c0c;
-    /* color: #d6d6d6; */
     font-size: 18px;
     margin-bottom: 25px;
     padding: 0.5rem 1rem 2rem;
+
+    @media screen and (max-width: 800px) {
+        font-size: 16px;
+    }
+
+    @media screen and (max-width: 550px) {
+        padding-top: 0;
+        font-size: 14px;
+    }
 `;
 
 export const EthosSection = styled.section`
-    /* background: #0c1013; */
     color: #0c1013;
     width: 100vw;
     padding-top: 100px;
+
+    @media screen and (max-width: 800px) {
+        padding-top: 0;
+    }
 `;
 
 export const EthoHeader = styled.h4`
-    color: white;
+    color: rgba(0, 0, 0, 0);
+    text-shadow: 0 0 0px white;
     text-transform: uppercase;
     font-size: 22px;
     font-weight: 700;
     margin: 0;
     z-index: 1;
+    transition: all 0.6s ease-in-out;
+
+    @media screen and (max-width: 800px) {
+        font-size: 14px;
+    }
+
+    @media screen and (max-width: 550px) {
+        display: none;
+    }
 `;
 export const EthoDescription = styled.p`
+    position: absolute;
+    left: 50%;
+    top: 65%;
+    transform: translate(-50%, -50%);
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
     font-weight: 400;
-    color: white;
-    font-size: 16px;
-    margin-top: 10px;
+    color: rgba(0, 0, 0, 0);
+    overflow: visible;
+    font-size: 18px;
+    padding: 0 6rem;
+    text-align: center;
     z-index: 1;
+    text-shadow: 0 0 50px transparent;
+    transition: all 0.5s ease-in-out;
+
+    span {
+        text-transform: uppercase;
+        font-size: 22px;
+        font-weight: 700;
+        top: 1rem;
+
+        @media screen and (max-width: 800px) {
+            font-size: 14px;
+        }
+    }
+
+    @media screen and (max-width: 800px) {
+        font-size: 14px;
+        padding: 0 2rem;
+    }
+
+    @media screen and (max-width: 550px) {
+        font-weight: 200;
+        color: white;
+        text-shadow: unset;
+        transition: none;
+        top: 50%;
+    }
+`;
+
+export const EthoOpacityOverlay = styled.div`
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+    background: black;
 `;
 
 export const EthoBackgroundImage = styled.div`
+    background: url(${props => props.bgImage});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
     position: absolute;
     height: 100%;
     width: 100%;
     object-fit: cover;
     top: 0;
     left: 0;
-    background: rgb(0 0 0 / 20%);
-    transition: all 0.5s ease-in-out;
+    opacity: 1;
+    transition: transform 10s cubic-bezier(0.25, 0.46, 0.45, 0.94),
+        opacity 0.4s ease-in-out;
+
+    &:after {
+        position: absolute;
+        content: "";
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        background: linear-gradient(
+            0deg,
+            rgba(4, 4, 4, 0.65),
+            rgba(54, 54, 54, 0.2)
+        );
+        transition: all 0.4s ease-in-out;
+    }
+
+    @media screen and (max-width: 550px) {
+        opacity: 0.4;
+    }
 `;
 
 export const IconsContainer = styled.div`
@@ -95,42 +191,26 @@ export const IconsContainer = styled.div`
     margin-top: 2rem;
 
     @media screen and (max-width: 800px) {
-        grid-template-columns: 1fr;
-        width: 43%;
+        grid-template-columns: 1fr 1fr;
         grid-gap: 0;
     }
 
     @media screen and (max-width: 550px) {
-        width: 85%;
+        grid-template-columns: 1fr;
     }
 `;
 
 export const EthoContainer = styled.div`
-    background: url(${props => props.bgImage});
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
+    overflow: hidden;
+    height: auto;
     position: relative;
     aspect-ratio: 1;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 0 4rem;
-    text-align: center;
-
-    @media screen and (max-width: 550px) {
-        padding: 0;
-    }
-
-    .etho-icon {
-        max-height: 150px;
-
-        @media screen and (max-width: 550px) {
-            width: 85%;
-            height: 100px;
-        }
-    }
+    justify-content: flex-end;
+    align-items: flex-start;
+    padding: 2rem 3rem;
+    text-align: left;
 
     img:nth-child(2) {
         width: 175px;
@@ -142,7 +222,26 @@ export const EthoContainer = styled.div`
     }
 
     &:hover ${EthoBackgroundImage} {
-        background: #000000c7;
+        transform: scale(1.3);
+        opacity: 0.4;
+    }
+
+    &:hover ${EthoHeader} {
+        color: rgba(0, 0, 0, 0);
+        text-shadow: 0 0 50px transparent;
+        overflow: visible;
+    }
+
+    &:hover ${EthoDescription} {
+        text-shadow: 0 0 0px white;
+        bottom: unset;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    @media screen and (max-width: 800px) {
+        padding: 1rem 2rem;
     }
 `;
 
